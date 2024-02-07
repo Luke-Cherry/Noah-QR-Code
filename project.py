005585ac2fadb373436c1707b6e8001f826e9e8a
MrVisneski <lcherry54321@gmail.com>
2024-02-07 10:20:23 +0000

    Testing layout to print all labels, current bug is making new page due to overlap

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-import sys
+import os
 from pyfiglet import Figlet
 from fpdf import FPDF
 import qrcode
@@ -12,28 +12,35 @@ def main():
     print(figlet.renderText("Noah QR Code"), end="")
     print("_" * 63)
     print()
-    #input("Please enter the name of the csv file: ")
+    #file_name = check_csv(input("Please enter the name of the csv file: "))
     
+    #read_csv(file_name)
+    read_csv("NoahDownload.csv")
 
-    #Move to read csv file
+
+#Check CSV valid, returns number of rows
+def check_csv(file_name):
+    while True:
+        if not file_name:
+            print("Error: Please enter a filename.")
+            file_name = input("Please enter the name of the CSV file: ")
+        elif not os.path.isfile(file_name):
+            print(f"Error: File '{file_name}' not found.")
+            file_name = input("Please enter the name of the CSV file: ")
+        else:
+            return file_name
+
+#Reads CSV file
+def read_csv(file_name):
     links_list = []
-    with open("NoahDownload1.csv", encoding="UTF-8-sig") as file:
+    with open(file_name, encoding="UTF-8-sig") as file:
         reader = csv.reader(file, delimiter=",")
         for row in reader:
             links_list.append(row)
 
     for row in links_list:
-        print(row)
+        #print(row)
         create_pdf(row[1], row[0] , create_qrcode(row[1]))
-
-
-#Check CSV valid, returns number of rows
-def check_csv():
-    ...
-
-#Reads CSV file
-def read_csv():
-    ...
 
 #Passes in link to convert it to a QR code 
 def create_qrcode(link):
@@ -48,24 +55,29 @@ def create_pdf(link, string, img):
     for i in range(1,6):
         pdf.line(0, i*49.5, 210, i*49.5)
 
-    #Turns QR object into image
-    pdf.image(img.get_image(), x=5, y=10, w=35, h=35)
+    for x in range(0, 1):
+        for y in range(0, 6):
+            x_offset = x * 105
+            y_offset = y * 49.5
+            print(x_offset, y_offset)
+            #Turns QR object into image
+            pdf.image(img.get_image(), x=5+x_offset, y=10+y_offset, w=35, h=35)
 
-    #Adds scan here text to the label
-    pdf.set_font("Helvetica", size=16, style="B")
-    pdf.text(x=8, y=10, text="Scan here for drug datasheet")
+            #Adds scan here text to the label
+            pdf.set_font("Helvetica", size=16, style="B")
+            pdf.text(x=8+x_offset, y=10+y_offset, text="Scan here for drug datasheet")
 
-    pdf.set_xy(x=40, y=15)
-    current_font = 20
-    pdf.set_font("Helvetica", size=current_font)
-    while pdf.get_string_width(string) > 170:
-        current_font -= 1
-        pdf.set_font("Helvetica", size=current_font)
-    pdf.multi_cell(w=60, text=string, align="C", max_line_height=pdf.font_size)
+            pdf.set_xy(x=40+x_offset, y=15+y_offset)
+            current_font = 15
+            pdf.set_font("Helvetica", size=current_font)
+            while pdf.get_string_width(string) > 170:
+                current_font -= 1
+                pdf.set_font("Helvetica", size=current_font)
+            #pdf.multi_cell(w=60, text=string, align="C", border=1)
 
-    pdf.set_xy(x=40, y=38)
-    pdf.set_font("Helvetica", size=10)
-    pdf.cell(w=60, text=f"noahcompendium.co.uk/?id=-{link}", align="C")
+            pdf.set_xy(x=40+x_offset, y=30+y_offset)
+            pdf.set_font("Helvetica", size=10)
+            pdf.cell(w=60, text=f"noahcompendium.co.uk/?id=-{link}", align="C", border=1)
 
     #Creates file in datasheet folder
     file_name = re.sub(r'[/\\:*?"<>|]', '', string)
